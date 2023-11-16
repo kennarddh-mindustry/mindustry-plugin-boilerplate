56a9e05cdc7d2d5f8015482b3b1d822302ce6509
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-11-16 16:47:21 +0700

    feat: Boilerplate

---
+++ src/kennarddh/Main.java
@@ -0,0 +1,12 @@
+package kennarddh;
+
+import arc.util.Log;
+import mindustry.mod.Plugin;
+
+@SuppressWarnings("unused")
+public class Main extends Plugin {
+    @Override
+    public void init() {
+        Log.info("[MindustryPlugin] Loaded");
+    }
+}
