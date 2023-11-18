c0abf4752cde5b6eae783b68fcef493b252a3758
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-11-18 16:49:51 +0700

    feat: Add boilerplate to name

--- src/kennarddh/Main.java
+++ src/kennarddh/Main.java
@@ -7,6 +7,6 @@ import mindustry.mod.Plugin;
 public class Main extends Plugin {
     @Override
     public void init() {
-        Log.info("[MindustryPlugin] Loaded");
+        Log.info("[MindustryPluginBoilerplate] Loaded");
     }
 }
